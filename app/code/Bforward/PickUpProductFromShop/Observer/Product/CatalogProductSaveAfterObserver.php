1ac8ac54145f498ad1f7f04eccf2e3ff825d74cf
bbakalov <bohdan.bakalov@everything5pounds.com>
2019-04-11 18:10:47 +0300

    was improved updating data of product stock show via dynamic rows in product edit

--- app/code/Bforward/PickUpProductFromShop/Observer/Product/CatalogProductSaveAfterObserver.php
+++ app/code/Bforward/PickUpProductFromShop/Observer/Product/CatalogProductSaveAfterObserver.php
@@ -2,20 +2,35 @@
 
 namespace Bforward\PickUpProductFromShop\Observer\Product;
 
+use Bforward\PickUpProductFromShop\Api\ProductShopStockRepositoryInterface;
+use Bforward\PickUpProductFromShop\Model\ResourceModel\ProductShopStock\CollectionFactory;
 use Magento\Framework\Event\Observer;
 use \Magento\Framework\Event\ObserverInterface;
 
 class CatalogProductSaveAfterObserver implements ObserverInterface
 {
+
     /**
      * @var \Bforward\PickUpProductFromShop\Model\ProductShopStockFactory
      */
     private $productShopStock;
+    /**
+     * @var \Bforward\PickUpProductFromShop\Api\ProductShopStockRepositoryInterface
+     */
+    private $productShopStockRepository;
+    /**
+     * @var \Bforward\PickUpProductFromShop\Model\ResourceModel\ProductShopStock\CollectionFactory
+     */
+    private $productShopStockCollectionFactory;
 
     public function __construct(
-        \Bforward\PickUpProductFromShop\Model\ProductShopStockFactory $productShopStock
+        \Bforward\PickUpProductFromShop\Model\ProductShopStockFactory $productShopStock,
+        ProductShopStockRepositoryInterface $productShopStockRepository,
+        CollectionFactory $productShopStockCollectionFactory
     ) {
-        $this->productShopStock   = $productShopStock;
+        $this->productShopStock                  = $productShopStock;
+        $this->productShopStockRepository        = $productShopStockRepository;
+        $this->productShopStockCollectionFactory = $productShopStockCollectionFactory;
     }
 
     /**
@@ -42,11 +57,22 @@ class CatalogProductSaveAfterObserver implements ObserverInterface
             if (\is_array($pickupShopData) && !empty($pickupShopData)) {
                 foreach ($pickupShopData as $shopData) {
                     unset($shopData['record_id']);
-                    $shopData['product_id'] = $productId;
-                    //TODO: save value to existing entity, instead of creating new
-                    $productShopStock       = $this->productShopStock->create();
-                    $productShopStock->setData($shopData);
-                    $productShopStock->save();
+                    //TODO: it's a little bit hardcode way, need to find best solution and implement it
+                    $productShopStock = $this->productShopStockCollectionFactory
+                        ->create()
+                        ->addFieldToFilter('product_id', $productId)
+                        ->addFieldToFilter('shop_id', $shopData['shop_id']);
+                    if ($item = $productShopStock->getFirstItem()) {
+                        //update product shop stock
+                        $shopStock = $this->productShopStockRepository->getById($item->getId());
+                        $shopStock->addData(['qty' => $shopData['qty']]);
+                    } else {
+                        //create new product shop stock
+                        $shopStock              = $this->productShopStock->create();
+                        $shopData['product_id'] = $productId;
+                        $shopStock->setData($shopData);
+                    }
+                    $this->productShopStockRepository->save($shopStock);
                 }
             }
         } catch (\Exception $e) {
